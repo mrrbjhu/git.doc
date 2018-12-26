7ce1a8a7182c0519fa8a68e00c2355e8b0d71f14
krka <krka92@bk.ru>
2018-12-26 13:50:39 +0700

    *

--- example.pas
+++ example.pas
@@ -9,18 +9,4 @@ begin
   Result := 'Hello World!';
 end;
 
-function goodBuy: String;
-begin
-  Result := 'Goodbuy Girlfriend!';
-end;
-
-function helloGirlfriend: String;
-begin
-  Result := 'Hello, Girlfriend';
-end;
-function helloBoyfriend: String;
-begin
-  Result := 'Hello, Boyfriend';
-end;
-
 end.
