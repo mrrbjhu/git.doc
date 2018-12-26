8dde1ffe55ee5f8cfaa272adec51528c781d24a3
krka <krka92@bk.ru>
2018-12-26 15:00:18 +0700

    # Conflicts: #	example.pas

--- example.pas
+++ example.pas
@@ -9,6 +9,11 @@ begin
   Result := 'Hello World!';
 end;
 
+function Summa(num1, num2: Integer): Integer;
+begin
+  Result := num1 + num2;
+end;
+
 function Multy(num1, num2: Integer): Integer;
 begin
   Result := num1 * num2;
