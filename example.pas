e811745903997efce73325bd5a690a7368a63d70
krka <krka92@bk.ru>
2018-12-26 17:56:20 +0700

    123

--- example.pas
+++ example.pas
@@ -11,7 +11,7 @@ end;
 
 function Summa(num1, num2: Integer): Integer;
 
-  if num1 = 0 then exit(num2);	
+  if num1 = 0 then exit(num2);	222222222222222222
   if num2 = 0 then exit(num1);
   Result := num1 + num2;
 end;
